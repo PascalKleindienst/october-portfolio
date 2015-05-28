13e74388dab34f0df143bd2c26ad0ff591f54a58
Pascal Kleindienst <mail@pascalkleindienst.de>
2015-05-28 11:14:21 +0200

    added translation for tags

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -12,9 +12,14 @@ return [
         'create_item' => 'portfolio item',
         'categories' => 'Categories',
         'create_category' => 'portfolio category',
+        'tags' => 'Tags',
+        'create_tag' => 'portfolio tag',
+        'delete_confirm' => 'Are you sure?',
+    ],
+
+    'permission' => [
         'access_items' => 'Manage the portfolio items',
         'access_categories' => 'Manage the blog categories',
-        'delete_confirm' => 'Are you sure?',
     ],
 
     'categories' => [
@@ -29,6 +34,20 @@ return [
         'slug_placeholder' => 'new-category-slug',
         'items' => 'Items',
         'delete_confirm' => 'Do you really want to delete this category?',
-        'return_to_categories' => 'Return to the blog category list',
+        'return_to_categories' => 'Return to the portfolio category list',
     ],
+
+    'tags' => [
+        'list_title' => 'Manage the portfolio tags',
+    ],
+
+    'tag' => [
+        'items' => 'Items',
+        'name' => 'Name',
+        'created' => 'Created at',
+        'updated' => 'Updated at',
+        'noItems' => 'No Items available',
+        'delete_confirm' => 'Do you really want to delete this tag?',
+        'return_to_tags' => 'Return to the portfolio tags list',
+    ]
 ];
